f08b2def1bd926c541c20451037806f298348858
Shouov Saha <shorov@newroztech.com>
2026-01-28 13:03:51 +0600

    feat(deploy): add netlify function build pipeline
    Add build script to copy functions, serverless-http dependency, and function
    entry point for netlify deployment. Update netlify config to use dist/functions
    directory for deployed functions.

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,6 +1,6 @@
 // Netlify Function entry point
 // This file is copied to dist/functions/ during build
-const { handler } = require('../dist/main.js');
+const { handler } = require('../../main.js');
 
 exports.handler = async (event, context) => {
   return handler(event, context);
